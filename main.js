c22398eca2443fc99ab34736be3468ad68ba1c57
Roberto Alves Martins <79550864+Robertogithu@users.noreply.github.com>
2023-09-12 15:02:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    
+    if (elemento && elemento.localName === 'audio' ){
+
+             elemento.play();
+    }else{
+        
+        console.log('Elemento não encontrado ou seletor inválido');
+
+    }
+    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para 
+for(let contador = 0; contador <listaDeTeclas.length; contador ++){
+    const tecla = listaDeTeclas[contador];
+    const  instrumento = tecla.classList[1]; 
+    
+    const idAudio = `#som_${instrumento}`;
+    
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        
+        console.log(evento.code);
+        if(evento.code === 'space'|| evento.code === 'Enter'|| evento.code === 'NumpadEnter'){
+        
+            tecla.classList.add('ativa');
+        }  
+        
+    
+    }
+
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+    
+}
